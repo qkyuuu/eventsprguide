3fdcdd185baaa9f98066682dec75ef3ee2bd1803
Jommel Pastoral <116055436+qkyuuu@users.noreply.github.com>
2026-02-03 14:19:01 +0800

    Update pr_feedback.php

--- pr-feedback/pr_feedback.php
+++ pr-feedback/pr_feedback.php
@@ -90,7 +90,7 @@ if ($appeal_query !== false) {
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css" rel="stylesheet">
 <link href="pr_feedback.css" rel="stylesheet">
-<link rel="stylesheet" href="/assets/css/bootstrap.min.css">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 </head>
 <body>
