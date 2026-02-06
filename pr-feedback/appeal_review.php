02cdb9da87cac48b2fcd9adbc41e653c29970070
Jommel Pastoral <116055436+qkyuuu@users.noreply.github.com>
2026-02-06 10:59:20 +0800

    Update appeal_review.php

--- pr-feedback/appeal_review.php
+++ pr-feedback/appeal_review.php
@@ -136,7 +136,7 @@ $reviewerImages = !empty($feedback['image_paths']) ? json_decode($feedback['imag
                 foreach ($reviewerImages['q'.$qid] as $img) {
                     // Generate Azure Blob URL
         $path = $azureBlobBaseUrl . rawurlencode($img);
-                    echo "<img src='$path' class='img-thumbnail preview-image' style='max-width:150px;margin:5px;cursor:pointer;' data-bs-toggle='modal' data-bs-target='#imageModal' data-img-src='$url'>";
+                    echo "<img src='$path' class='img-thumbnail preview-image' style='max-width:150px;margin:5px;cursor:pointer;' data-bs-toggle='modal' data-bs-target='#imageModal' data-img-src='$path'>";
                 }
             } else {
                 echo "<p>No images uploaded.</p>";
