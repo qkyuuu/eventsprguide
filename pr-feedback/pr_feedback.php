ccd70bfbf45e97716466b5648cbd6fe1b206d34d
Jommel Pastoral <116055436+qkyuuu@users.noreply.github.com>
2026-02-06 09:39:25 +0800

    Update pr_feedback.php

--- pr-feedback/pr_feedback.php
+++ pr-feedback/pr_feedback.php
@@ -90,6 +90,7 @@ if ($appeal_query !== false) {
 <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css" rel="stylesheet">
 <link href="pr_feedback.css" rel="stylesheet">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+<link rel="icon" href="https://raw.githubusercontent.com/qkyuuu/eventsprguide/main/img/dashboard.png" type="image/svg+xml" />
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 </head>
 <body>
